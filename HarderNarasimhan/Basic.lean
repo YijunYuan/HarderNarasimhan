321b9210036ce75674a65946f77a740f299072a6
Yijun Yuan <941201yuan@gmail.com>
2025-05-26 19:16:39 +0800

    update

--- HarderNarasimhan/Basic.lean
+++ HarderNarasimhan/Basic.lean
@@ -4,13 +4,16 @@ import Mathlib.Algebra.Group.Subgroup.Defs
 
 import HarderNarasimhan.Interval
 
-def VAI (ℒ : Type) [Nontrivial ℒ] [PartialOrder ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (μ : ℒ × ℒ → S) (IBot : ℒ) (ITop : ℒ) (x : ℒ) (_ : IBot ≤ x ∧ x < ITop) : Set S := μ '' {p : ℒ × ℒ | p.1 = x ∧ (IBot ≤ p.2 ∧ p.2 ≤ ITop) ∧ p.1 < p.2}
+-- VAI does `NOT` make sense if `IBot ≤ x ∧ x < ITop` is not satisfied.
+def VAI (ℒ : Type) [Nontrivial ℒ] [PartialOrder ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (μ : ℒ × ℒ → S) (IBot : ℒ) (ITop : ℒ) (x : ℒ) : Set S := μ '' {p : ℒ × ℒ | p.1 = x ∧ (IBot ≤ x ∧ x < ITop) ∧ (IBot ≤ p.2 ∧ p.2 ≤ ITop) ∧ x < p.2}
 
-def μmaxI (ℒ : Type) [Nontrivial ℒ] [PartialOrder ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (μ : ℒ × ℒ → S) (a : ℒ) (y : ℒ) (h : a < y) : S := sSup (VAI ℒ S μ ⊥ y a ⟨bot_le,h⟩)
+-- μmaxI does `NOT` make sense if `I.1 < I.2` is not satisfied.
+def μmaxI (ℒ : Type) [Nontrivial ℒ] [PartialOrder ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (μ : ℒ × ℒ → S) (I : ℒ × ℒ) : S := sSup (VAI ℒ S μ ⊥ I.2 I.1)
 
-def μAI (ℒ : Type) [Nontrivial ℒ] [PartialOrder ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (μ : ℒ × ℒ → S) (IBot : ℒ) (ITop : ℒ) (_ : IBot < ITop) : S := sInf {μmaxI ℒ S μ a ITop ha.2 | (a : ℒ) (ha : IBot ≤ a ∧ a < ITop)}
+-- μAI does `NOT` make sense if `I.1 < I.2` is not satisfied.
+def μAI (ℒ : Type) [Nontrivial ℒ] [PartialOrder ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (μ : ℒ × ℒ → S) (I : ℒ × ℒ) : S := sInf {μmaxI ℒ S μ (a,I.2) | (a : ℒ) (_ : I.1 ≤ a ∧ a < I.2)}
 
-def μAstar (ℒ : Type) [Nontrivial ℒ] [PartialOrder ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (μ : ℒ × ℒ → S) : S := μAI ℒ S μ ⊥ ⊤ bot_lt_top
+def μAstar (ℒ : Type) [Nontrivial ℒ] [PartialOrder ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (μ : ℒ × ℒ → S) : S := μAI ℒ S μ (⊥,⊤)
 
 
 --def supVₐ (ℒ: Type) [PartialOrder ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (μ : ℒ × ℒ → S) (x : ℒ) : S := sSup (Vₐ ℒ S μ x)
--- HarderNarasimhan/Convexity.lean
+++ HarderNarasimhan/Convexity.lean
@@ -2,9 +2,6 @@ import HarderNarasimhan.Basic
 
 def IsConvexI {ℒ : Type} [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ] {S : Type} [CompleteLattice S] (IBot : ℒ) (ITop : ℒ) (_ : IBot < ITop) (μ : ℒ × ℒ → S) : Prop := ∀ x : ℒ, ∀ y : ℒ, IBot ≤ x ∧ x ≤ ITop ∧ IBot ≤ y ∧ y ≤ ITop ∧ ¬ x ≤ y → μ (x ⊓ y,x) ≤ μ (y,x ⊔ y)
 
---lemma lem2d4IH₃ {ℒ : Type} [Lattice ℒ] (x : ℒ) (w : ℒ) (t : ℒ) (h₁ : ¬ x ≤ w) (h₂ : x ⊔ w ≤ t) : w < t := lt_iff_le_and_ne.2
---  ⟨ le_trans le_sup_right h₂,
---    fun a ↦ Eq.mp (congrArg (fun _a ↦ ¬x ≤ _a) a) h₁ (sup_le_iff.1 h₂).left⟩
 
 lemma lem2d4I (ℒ : Type) [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ]
 (S : Type) [CompleteLattice S]
@@ -18,13 +15,9 @@ lemma lem2d4I (ℒ : Type) [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ]
 (hut : u < t)
 (huxw : u ≤ x ⊓ w)
 (hxwt : x ⊔ w ≤ t) :
-(μAI ℒ S μ u x (lt_of_le_of_lt huxw (inf_lt_left.2 hxw))) ≤ μmaxI ℒ S μ (x ⊓ w) x  (inf_lt_left.2 hxw) ∧
-μmaxI ℒ S μ (x ⊓ w) x (inf_lt_left.2 hxw) ≤ μmaxI ℒ S μ w t (lt_iff_le_and_ne.2
-  ⟨ le_trans le_sup_right hxwt,
-    fun a ↦ Eq.mp (congrArg (fun _a ↦ ¬x ≤ _a) a) hxw (sup_le_iff.1 hxwt).left⟩) ∧
-(μAI ℒ S μ u x (lt_of_le_of_lt huxw (inf_lt_left.2 hxw))) ≤ μAI ℒ S μ w t (lt_iff_le_and_ne.2
-  ⟨ le_trans le_sup_right hxwt,
-    fun a ↦ Eq.mp (congrArg (fun _a ↦ ¬x ≤ _a) a) hxw (sup_le_iff.1 hxwt).left⟩)
+μAI ℒ S μ (u , x) ≤ μmaxI ℒ S μ (x ⊓ w , x) ∧
+μmaxI ℒ S μ (x ⊓ w , x) ≤ μmaxI ℒ S μ (w , t) ∧
+μAI ℒ S μ (u , x) ≤ μAI ℒ S μ (w , t)
 := sorry
 
 lemma lem2d4 (ℒ : Type) [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ]
@@ -38,15 +31,52 @@ lemma lem2d4 (ℒ : Type) [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ]
 (hut : u < t)
 (huxw : u ≤ x ⊓ w)
 (hxwt : x ⊔ w ≤ t) :
-(μAI ℒ S μ u x (lt_of_le_of_lt huxw (inf_lt_left.2 hxw))) ≤ μmaxI ℒ S μ (x ⊓ w) x  (inf_lt_left.2 hxw) ∧
-μmaxI ℒ S μ (x ⊓ w) x (inf_lt_left.2 hxw) ≤ μmaxI ℒ S μ w t (lt_iff_le_and_ne.2
-  ⟨ le_trans le_sup_right hxwt,
-    fun a ↦ Eq.mp (congrArg (fun _a ↦ ¬x ≤ _a) a) hxw (sup_le_iff.1 hxwt).left⟩) ∧
-(μAI ℒ S μ u x (lt_of_le_of_lt huxw (inf_lt_left.2 hxw))) ≤ μAI ℒ S μ w t (lt_iff_le_and_ne.2
-  ⟨ le_trans le_sup_right hxwt,
-    fun a ↦ Eq.mp (congrArg (fun _a ↦ ¬x ≤ _a) a) hxw (sup_le_iff.1 hxwt).left⟩)
+μAI ℒ S μ (u , x) ≤ μmaxI ℒ S μ (x ⊓ w , x) ∧
+μmaxI ℒ S μ (x ⊓ w , x) ≤ μmaxI ℒ S μ (w , t) ∧
+μAI ℒ S μ (u , x) ≤ μAI ℒ S μ (w , t)
 := lem2d4I ℒ S ⊥ ⊤ bot_lt_top μ hμcvx x ⟨bot_lt_iff_ne_bot.2 hx, le_top⟩ w
   ⟨bot_lt_iff_ne_bot.2 hx, le_top⟩ hxw u ⟨bot_le, le_top⟩ t ⟨bot_le, le_top⟩ hut huxw hxwt
 
---lemma rmk2d5a (ℒ : Type) [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (IBot : ℒ) (ITop : ℒ) (h : IBot < ITop)
---(μ : ℒ × ℒ → S) (hμcvx : IsConvexI IBot ITop h μ) : IsConvexI IBot ITop h () := sorry
+lemma u_lt_x {ℒ : Type} [Lattice ℒ] (x : ℒ) (w : ℒ) (u : ℒ) (hxw : ¬ x ≤ w) (huxw : u ≤ x ⊓ w) : u < x := lt_of_le_of_lt huxw (inf_lt_left.2 hxw)
+
+lemma x_inf_w_lt_x {ℒ : Type} [Lattice ℒ] (x : ℒ) (w : ℒ) (hxw : ¬ x ≤ w) : x ⊓ w < x := inf_lt_left.2 hxw
+
+lemma w_lt_t {ℒ : Type} [Lattice ℒ] (x : ℒ) (w : ℒ) (t : ℒ) (h₁ : ¬ x ≤ w) (h₂ : x ⊔ w ≤ t) : w < t := lt_iff_le_and_ne.2
+  ⟨ le_trans le_sup_right h₂,
+    fun a ↦ Eq.mp (congrArg (fun _a ↦ ¬x ≤ _a) a) h₁ (sup_le_iff.1 h₂).left⟩
+
+
+lemma rmk2d5a (ℒ : Type) [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (IBot : ℒ) (ITop : ℒ) (h : IBot < ITop)
+(μ : ℒ × ℒ → S) (hμcvx : IsConvexI IBot ITop h μ) : IsConvexI IBot ITop h (μmaxI ℒ S μ)  := sorry
+
+lemma rmk2d5b (ℒ : Type) [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (IBot : ℒ) (ITop : ℒ) (h : IBot < ITop) (μ : ℒ × ℒ → S) (hμcvx : IsConvexI IBot ITop h μ) : μmaxI ℒ S μ (IBot , ITop) = μmaxI ℒ S (μmaxI ℒ S μ) (IBot , ITop) := sorry
+
+lemma prop2d6main (ℒ : Type) [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (IBot : ℒ) (μ : ℒ × ℒ → S) (x : ℒ) (y : ℒ) (z : ℒ) (h : x < y ∧ y < z) : μAI ℒ S μ (y , z) ≥ μAI ℒ S μ (x , z) :=
+sorry
+
+lemma prop2d6a (ℒ : Type) [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (IBot : ℒ) (μ : ℒ × ℒ → S) (IBot : ℒ) (ITop : ℒ) (h : IBot < ITop) (μ : ℒ × ℒ → S) (hμcvx : IsConvexI IBot ITop h μ) (x : ℒ) (y : ℒ) (z : ℒ) (h : IBot ≤ x ∧ x < y ∧ y < z ∧ z ≤ ITop) :
+μAI ℒ S μ (x , z) ≥ (μAI ℒ S μ (x , y)) ⊓ (μAI ℒ S μ (y , z)) :=
+sorry
+
+lemma prop2d6b1 (ℒ : Type) [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (IBot : ℒ) (μ : ℒ × ℒ → S) (IBot : ℒ) (ITop : ℒ) (h : IBot < ITop) (μ : ℒ × ℒ → S) (hμcvx : IsConvexI IBot ITop h μ) (x : ℒ) (y : ℒ) (z : ℒ) (h : IBot ≤ x ∧ x < y ∧ y < z ∧ z ≤ ITop) (h' : μAI ℒ S μ (x , y) ≥ μAI ℒ S μ (y , z)) :
+μAI ℒ S μ (y , z) = μAI ℒ S μ (x , z) :=
+sorry
+
+lemma prop2d6b2 (ℒ : Type) [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (IBot : ℒ) (μ : ℒ × ℒ → S) (IBot : ℒ) (ITop : ℒ) (h : IBot < ITop) (μ : ℒ × ℒ → S) (hμcvx : IsConvexI IBot ITop h μ) (x : ℒ) (y : ℒ) (z : ℒ) (h : IBot ≤ x ∧ x < y ∧ y < z ∧ z ≤ ITop) (h' : μAI ℒ S μ (x , y) < μAI ℒ S μ (y , z)) :
+μAI ℒ S μ (x , y) ≤ μAI ℒ S μ (x , z) ∧
+μAI ℒ S μ (x , z) ≤ μAI ℒ S μ (y , z) :=
+sorry
+
+lemma prop2d6c (ℒ : Type) [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (IBot : ℒ) (μ : ℒ × ℒ → S) (IBot : ℒ) (ITop : ℒ) (h : IBot < ITop) (μ : ℒ × ℒ → S) (hμcvx : IsConvexI IBot ITop h μ) (x : ℒ) (y : ℒ) (z : ℒ) (h : IBot ≤ x ∧ x < y ∧ y < z ∧ z ≤ ITop) (h' : μAI ℒ S μ (x , y) ≤ μAI ℒ S μ (y , z) ∨ μAI ℒ S μ (x , y) ≥ μAI ℒ S μ (y , z) ∨ (∃ u : S, u = μAI ℒ S μ (x , z))) :
+(μAI ℒ S μ (y , z) = μAI ℒ S μ (x , z)) ∨ (μAI ℒ S μ (x , y) ≤ μAI ℒ S μ (x , z) ∧ μAI ℒ S μ (x , z) < μAI ℒ S μ (y , z)) :=
+sorry
+
+lemma rmk2d7 (ℒ : Type) [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] [LinearOrder S] (μ : ℒ × ℒ → S) (hμcvx : IsConvexI ⊥ ⊤ bot_lt_top μ) (x : ℒ) (h : ⊥ < x ∧ x < ⊤) (h' : μAI ℒ S μ (⊥ , x) > μAI ℒ S μ (⊥ , ⊤) ) :
+μAI ℒ S μ (x , ⊤) = μAI ℒ S μ (⊥ , ⊤) :=
+sorry
+
+lemma prop2d8a (ℒ : Type) [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (μ : ℒ × ℒ → S) (IBot : ℒ) (ITop : ℒ) (h : IBot < ITop) (hμcvx : IsConvexI IBot ITop h μ) (x : ℒ) (y : ℒ) (u : ℒ) (h' : IBot ≤ u ∧ u < x ∧ u < y ∧ x ≤ ITop ∧ y ≤ ITop) :
+μAI ℒ S μ (u, x ⊔ y) ≥ μAI ℒ S μ (u , x) ⊓ μAI ℒ S μ (u , y) :=
+sorry
+
+lemma prop2d8b (ℒ : Type) [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (μ : ℒ × ℒ → S) (IBot : ℒ) (ITop : ℒ) (h : IBot < ITop) (hμcvx : IsConvexI IBot ITop h μ) (x : ℒ) (y : ℒ) (u : ℒ) (h' : IBot ≤ u ∧ u < x ∧ u < y ∧ x ≤ ITop ∧ y ≤ ITop) (hcpb: μAI ℒ S μ (u , x) ≤ μAI ℒ S μ (u , y) ∨ μAI ℒ S μ (u , x) ≥ μAI ℒ S μ (u , y) ∨ (∃ v : S, v = μAI ℒ S μ (u , x ⊔ y))): (μAI ℒ S μ (u , x ⊔ y) ≥ μAI ℒ S μ (u , x)) ∨ (μAI ℒ S μ (u , x ⊔ y) ≥ μAI ℒ S μ (u , x)) := sorry
