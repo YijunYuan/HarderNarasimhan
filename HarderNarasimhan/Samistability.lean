00a629aca43670d1341c8224f4d98eb11acfff43
Yijun Yuan <941201yuan@gmail.com>
2025-05-30 21:42:48 +0800

    添加 S₁I 和 S₂I 定义，更新 prop3d2 和 cor3d3 以提高可读性和一致性

--- HarderNarasimhan/Samistability.lean
+++ HarderNarasimhan/Samistability.lean
@@ -11,4 +11,14 @@ def μDCCond (ℒ : Type) [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ] {S :
 
 lemma clearly (ℒ : Type) [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (μ : ℒ × ℒ → S) (I : ℒ × ℒ) (_ : I.1 < I.2) (h : μDCCond ℒ μ) (x : ℒ) (hx : x ≠ ⊤) : μDCCondI ℒ μ (x , ⊤) (lt_top_iff_ne_top.2 hx) := sorry
 
-lemma prop3d2 (ℒ : Type) [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (μ : ℒ × ℒ → S) (I : ℒ × ℒ) (_ : I.1 < I.2) (h : μDCCond ℒ μ) (x : ℒ) (z : ℒ) (h : I.1 ≤ x ∧ x < z ∧ z ≤ I.2) (h' : μA μ (x , z) = ⊤) (a : ℒ) (ha : I.1 < a ∧ a < x) : μA μ (a , x) < μA μ (a , z)  := sorry
+lemma prop3d2 (ℒ : Type) [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (μ : ℒ × ℒ → S) (I : ℒ × ℒ) (_ : I.1 < I.2) (h : μDCCond ℒ μ) (x : ℒ) (z : ℒ) (h : I.1 ≤ x ∧ x < z ∧ z ≤ I.2) (h' : μA μ (x , z) = ⊤) (a : ℒ) (ha : I.1 < a ∧ a < x) : μA μ (a , x) ≤ μA μ (a , z)  := sorry
+
+lemma cor3d3 (ℒ : Type) [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (μ : ℒ × ℒ → S) (I : ℒ × ℒ) (hI : I.1 < I.2) (f : ℕ → ℒ) (hfI : ∀ n : ℕ, InInterval I (f n)) (hfm : ∀ n : ℕ, f n > f (n + 1)) (h : ∃ N : ℕ, μA μ (f (N + 1) , f N) = ⊤) : μDCCondI ℒ μ I hI := sorry
+
+
+
+def S₁I {ℒ : Type} [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ] {S : Type} [CompleteLattice S] (μ : ℒ × ℒ → S) (I : ℒ × ℒ) (_ : I.1 < I.2) (x : ℒ) (_ : InInterval I x) : Prop := ∀ y : ℒ, y ≠ ⊥ → ¬ μA μ (I.1 , y) > μA μ (I.1 , x)
+
+def S₂I {ℒ : Type} [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ] {S : Type} [CompleteLattice S] (μ : ℒ × ℒ → S) (I : ℒ × ℒ) (_ : I.1 < I.2) (x : ℒ) (_ : InInterval I x) : Prop := ∀ y : ℒ, y ≠ ⊥ → μA μ (I.1 , y) = μA μ (I.1 , x) → y ≤ x
+
+lemma prop3d4 (ℒ : Type) [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ] (S : Type) [CompleteLattice S] (μ : ℒ × ℒ → S) (I : ℒ × ℒ) (hI : I.1 < I.2) (hACC : ACCondI ℒ I hI) (hμDCC : μDCCondI ℒ μ I hI) : ∃ x : ℒ, ∃ hxI : InInterval I x,  (x ≠ I.1) ∧ (S₁I μ I hI x hxI) ∧ (S₂I μ I hI x hxI) := sorry
