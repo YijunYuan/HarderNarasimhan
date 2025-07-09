a41abe02a31741a3749ded83ced73f3e8ca70f7d
Yijun Yuan <941201yuan@gmail.com>
2025-07-09 23:11:46 +0800

    update

--- HarderNarasimhan/SlopeLike/main.lean
+++ HarderNarasimhan/SlopeLike/main.lean
@@ -63,7 +63,7 @@ SlopeLike μ ↔ ∀ (x y z : ℒ), (h : x < y ∧ y < z) → (
       cases' this with this this <;> [exact le_of_lt this.1; exact le_of_eq this.1.symm]
 
 
-class TotallyOrderedRealVectorSpace (V : Type) extends AddCommMonoid V, Module ℝ V, LinearOrder V where
+class TotallyOrderedRealVectorSpace (V : Type) extends AddCommGroup V, Module ℝ V, LinearOrder V where
   add_le : ∀ {y z : V} (x : V), y ≤ z → x + y ≤ x + z
   scalar_le : ∀ {y z : V} (c : ℝ), c ≥ 0 → c • y ≤ c • z
 
@@ -74,8 +74,29 @@ noncomputable def μQuotient {ℒ : Type} [Nontrivial ℒ] [Lattice ℒ] [Bounde
 (d : {p :ℒ × ℒ // p.1 < p.2} → V): {p :ℒ × ℒ // p.1 < p.2} → DedekindMacNeilleCompletion V := fun
     z ↦
   if _ : r z > 0 then
-    ↑((r z)⁻¹ • d z)
+    coe' ((r z)⁻¹ • d z)
   else ⊤
+
+
+lemma μQuotient_helper {ℒ : Type} [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ]
+{V : Type} [TotallyOrderedRealVectorSpace V]
+(r : {p :ℒ × ℒ // p.1 < p.2} → NNReal)
+(d : {p :ℒ × ℒ // p.1 < p.2} → V): ∀ z : {p :ℒ × ℒ // p.1 < p.2}, r z > 0 → ∃ (μ : V), (μQuotient r d) z = coe' μ ∧ (r z) • μ = (d z) := by
+  intro z h
+  use ((r z)⁻¹ • d z)
+  constructor
+  · simp [μQuotient, coe', h]
+  · exact smul_inv_smul₀ (by aesop) (d z)
+
+
+noncomputable instance : Group {p : NNReal // p > 0} where
+  one := ⟨⟨1,by linarith⟩, by aesop⟩
+  mul := fun a b ↦ ⟨a.1 * b.1, by aesop⟩
+  inv := fun x ↦ ⟨x⁻¹, by aesop⟩
+  one_mul := fun x ↦ by sorry
+  mul_one := fun x ↦ by sorry
+  mul_assoc := fun x y z ↦ by sorry
+  inv_mul_cancel := fun x ↦ sorry
 
 
 lemma prop4d8 {ℒ : Type} [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ]
@@ -100,5 +121,27 @@ lemma prop4d8 {ℒ : Type} [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ]
     aesop
   · by_cases h'' : r ⟨(x, y), h.1⟩ > 0 ∧ r ⟨(y, z), h.2⟩ > 0
     · --have : μ ⟨(x,z), lt_trans h.1 h.2⟩
-      sorry
+      rcases μQuotient_helper r d ⟨(x, y), h.1⟩ h''.1 with ⟨μxy,⟨hxy₁,hxy₂⟩⟩
+      rcases μQuotient_helper r d ⟨(y, z), h.2⟩ h''.2 with ⟨μyz,⟨hyz₁,hyz₂⟩⟩
+      rcases μQuotient_helper r d ⟨(x, z), lt_trans h.1 h.2⟩ h' with ⟨μxz,⟨hxz₁,hxz₂⟩⟩
+      have : r ⟨(x, y), h.1⟩ • μxz + r ⟨(y, z), h.2⟩ • μxz = r ⟨(x, y), h.1⟩ • μxy + r ⟨(y, z), h.2⟩ • μyz := add_smul (r ⟨(x, y), h.1⟩) (r ⟨(y, z), h.2⟩) μxz ▸ (h₁ x y z h).2 ▸ hxy₂ ▸ hyz₂ ▸ hxz₂ ▸ (h₁ x y z h).1
+      simp [μ,hxy₁,hyz₁,hxz₁]
+      by_cases hs : μxy < μxz
+      · left
+        sorry
+      · by_cases hs' : μxy = μxz
+        · right
+          right
+          refine ⟨hs',?_⟩
+          simp [hs'] at this
+          have : (r ⟨(y, z), h.2⟩)⁻¹ • (r ⟨(y, z), h.2⟩) • μxz = (r ⟨(y, z), h.2⟩)⁻¹ • (r ⟨(y, z), h.2⟩) • μyz := by
+            exact congrArg (HSMul.hSMul (r ⟨(y, z), h.right⟩)⁻¹) this
+
+          --rw [← smul_assoc] at this
+          --have fuck : r ⟨(x, y), h.1⟩ ≠ 0 := by aesop
+
+          --rw [inv_smul_smul (⟨(r ⟨(y, z), h.2⟩), h''.2⟩:{p : NNReal // p > 0}) μyz] at this
+          --apply NNReal.mul_eq_mul_left
+          sorry
+        · sorry
     · sorry
