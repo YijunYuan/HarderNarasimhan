493a0b01be1245a66b96a5702bbcff43ece54ecb
YijunYuan <941201yuan@gmail.com>
2025-07-12 12:20:03 +0800

    优化 prop4d6 引理的逻辑，简化条件判断，增强代码的可读性

--- HarderNarasimhan/SlopeLike/Impl.lean
+++ HarderNarasimhan/SlopeLike/Impl.lean
@@ -32,9 +32,7 @@ SlopeLike μ ↔ ∀ (x y z : ℒ), (h : x < y ∧ y < z) → (
       cases' this with this this <;> [exact le_of_lt this.1; exact le_of_eq this.1]
     · have : μ ⟨(x, y), h.1⟩ < μ ⟨(x, z), lt_trans h.1 h.2⟩ ↔ ¬¬ μ ⟨(x, y), h.1⟩ < μ ⟨(x, z), lt_trans h.1 h.2⟩ := Iff.symm not_not
       rw [this]
-      refine imp_iff_not_or.1 fun h' ↦ ?_
-      have : μ ⟨(x, y), h.1⟩ > μ ⟨(x, z), lt_trans h.1 h.2⟩ ∧ μ ⟨(x, z), lt_trans h.1 h.2⟩ > μ ⟨(y, z), h.2⟩ ∨ μ ⟨(x, y), h.1⟩ = μ ⟨(x, z), lt_trans h.1 h.2⟩ ∧ μ ⟨(x, z), lt_trans h.1 h.2⟩ = μ ⟨(y, z), h.2⟩ := by tauto
-      cases' this with this this <;> [exact le_of_lt this.2; exact le_of_eq this.2.symm]
+      exact imp_iff_not_or.1 fun h' ↦ by cases' (by tauto : μ ⟨(x, y), h.1⟩ > μ ⟨(x, z), lt_trans h.1 h.2⟩ ∧ μ ⟨(x, z), lt_trans h.1 h.2⟩ > μ ⟨(y, z), h.2⟩ ∨ μ ⟨(x, y), h.1⟩ = μ ⟨(x, z), lt_trans h.1 h.2⟩ ∧ μ ⟨(x, z), lt_trans h.1 h.2⟩ = μ ⟨(y, z), h.2⟩) with this this <;> [exact le_of_lt this.2; exact le_of_eq this.2.symm]
     · have : μ ⟨(x, z), lt_trans h.1 h.2⟩ < μ ⟨(x, y), h.1⟩ ↔ ¬¬ μ ⟨(x, z), lt_trans h.1 h.2⟩ < μ ⟨(x, y), h.1⟩ := Iff.symm not_not
       rw [this]
       refine imp_iff_not_or.1 fun h' ↦ ?_
@@ -42,9 +40,7 @@ SlopeLike μ ↔ ∀ (x y z : ℒ), (h : x < y ∧ y < z) → (
       cases' this with this this <;> [exact le_of_lt this.2; exact le_of_eq this.2]
     · have : μ ⟨(x, z), lt_trans h.1 h.2⟩ < μ ⟨(y, z), h.2⟩ ↔ ¬¬ μ ⟨(x, z), lt_trans h.1 h.2⟩ < μ ⟨(y, z), h.2⟩ := Iff.symm not_not
       rw [this]
-      refine imp_iff_or_not.1 fun h' ↦ ?_
-      have : μ ⟨(x, y), h.1⟩ > μ ⟨(x, z), lt_trans h.1 h.2⟩ ∧ μ ⟨(x, z), lt_trans h.1 h.2⟩ > μ ⟨(y, z), h.2⟩ ∨ μ ⟨(x, y), h.1⟩ = μ ⟨(x, z), lt_trans h.1 h.2⟩ ∧ μ ⟨(x, z), lt_trans h.1 h.2⟩ = μ ⟨(y, z), h.2⟩ := by tauto
-      cases' this with this this <;> [exact le_of_lt this.1; exact le_of_eq this.1.symm]
+      exact imp_iff_or_not.1 fun h' ↦ by cases' (by tauto : μ ⟨(x, y), h.1⟩ > μ ⟨(x, z), lt_trans h.1 h.2⟩ ∧ μ ⟨(x, z), lt_trans h.1 h.2⟩ > μ ⟨(y, z), h.2⟩ ∨ μ ⟨(x, y), h.1⟩ = μ ⟨(x, z), lt_trans h.1 h.2⟩ ∧ μ ⟨(x, z), lt_trans h.1 h.2⟩ = μ ⟨(y, z), h.2⟩) with this this <;> [exact le_of_lt this.1; exact le_of_eq this.1.symm]
 
 
 lemma not_top_of_Nontrivial_TotallyOrderedRealVectorSpace {V : Type} [TotallyOrderedRealVectorSpace V] [hnt: Nontrivial V] : ∀ v : V, coe' v < (⊤ : DedekindMacNeilleCompletion V) := by
@@ -89,58 +85,38 @@ lemma prop4d8 {ℒ : Type} [Nontrivial ℒ] [Lattice ℒ] [BoundedOrder ℒ]
     · rcases μQuotient_helper r d ⟨(x, y), h.1⟩ h''.1 with ⟨μxy,⟨hxy₁,hxy₂⟩⟩
       rcases μQuotient_helper r d ⟨(y, z), h.2⟩ h''.2 with ⟨μyz,⟨hyz₁,hyz₂⟩⟩
       rcases μQuotient_helper r d ⟨(x, z), lt_trans h.1 h.2⟩ h' with ⟨μxz,⟨hxz₁,hxz₂⟩⟩
-      have : r ⟨(x, y), h.1⟩ • μxz + r ⟨(y, z), h.2⟩ • μxz = r ⟨(x, y), h.1⟩ • μxy + r ⟨(y, z), h.2⟩ • μyz := add_smul (r ⟨(x, y), h.1⟩) (r ⟨(y, z), h.2⟩) μxz ▸ (h₁ x y z h).2 ▸ hxy₂ ▸ hyz₂ ▸ hxz₂ ▸ (h₁ x y z h).1
+      have := add_smul (r ⟨(x, y), h.1⟩) (r ⟨(y, z), h.2⟩) μxz ▸ (h₁ x y z h).2 ▸ hxy₂ ▸ hyz₂ ▸ hxz₂ ▸ (h₁ x y z h).1
       simp [μ,hxy₁,hyz₁,hxz₁]
       by_cases hs : μxy < μxz
-      · refine Or.inl ⟨hs,?_⟩
-        have hs := (eq_sub_of_add_eq this) ▸ (smul_lt_smul_iff_of_pos_left h''.1).2 hs
-        rw [lt_sub_iff_add_lt,add_lt_add_iff_left] at hs
-        exact (smul_lt_smul_iff_of_pos_left h''.2).1 hs
+      · exact Or.inl ⟨hs,(smul_lt_smul_iff_of_pos_left h''.2).1 <| (add_lt_add_iff_left <| r ⟨(x, y), h.1⟩ • μxy).1 <| lt_sub_iff_add_lt.1 <| (eq_sub_of_add_eq this) ▸ (smul_lt_smul_iff_of_pos_left h''.1).2 hs⟩
       · by_cases hs' : μxy = μxz
         · refine Or.inr <| Or.inr <| ⟨hs',?_⟩
           simp [hs'] at this
-          have : (r ⟨(y, z), h.2⟩)⁻¹ • (r ⟨(y, z), h.2⟩) • μxz = (r ⟨(y, z), h.2⟩)⁻¹ • (r ⟨(y, z), h.2⟩) • μyz := congrArg (HSMul.hSMul (r ⟨(y, z), h.right⟩)⁻¹) this
-          rw [← smul_assoc,← smul_assoc] at this
-          simp at this
-          have invt : Invertible (r ⟨(y, z), h.2⟩) := by
-            refine { invOf := ?_, invOf_mul_self := ?_, mul_invOf_self := ?_ }
-            use (r ⟨(y, z), h.2⟩)⁻¹
-            · aesop
-            · exact (mul_eq_one_iff_eq_inv₀ <| ne_of_gt h''.2).2 rfl
-            · exact (mul_eq_one_iff_inv_eq₀ <| ne_of_gt h''.2).2 rfl
-          rw [inv_mul_cancel_of_invertible <| r ⟨(y, z), h.2⟩,one_smul,one_smul] at this
-          exact this
+          exact smul_right_injective V (ne_of_lt h''.2).symm this
         · have hs' : μxz < μxy := lt_of_not_le (Eq.mpr (id (congrArg (fun _a ↦ ¬_a) (propext le_iff_eq_or_lt))) (not_or.mpr ⟨hs', hs⟩))
-          refine Or.inr <| Or.inl <| ⟨hs',?_⟩
-          have hs' := (eq_sub_of_add_eq this) ▸ (smul_lt_smul_iff_of_pos_left h''.1).2 hs'
-          rw [sub_lt_iff_lt_add,add_lt_add_iff_left] at hs'
-          exact (smul_lt_smul_iff_of_pos_left h''.2).1 hs'
+          exact Or.inr <| Or.inl <| ⟨hs',(smul_lt_smul_iff_of_pos_left h''.2).1 <| (add_lt_add_iff_left <| r ⟨(x, y), h.1⟩ • μxy).1 <| sub_lt_iff_lt_add.1 <| (eq_sub_of_add_eq this) ▸ (smul_lt_smul_iff_of_pos_left h''.1).2 hs'⟩
     · by_cases h''' : r ⟨(x, y), h.1⟩ = 0 ∧ r ⟨(y, z), h.2⟩ > 0
       · have h2 : μ ⟨(x, y), h.1⟩ = ⊤ := by simp [μ, μQuotient, h'''.1]
         have h4 := (zero_add <| r ⟨(y, z), h.2⟩) ▸ h'''.1 ▸ (h₁ x y z h).2
-        have h3 : μ ⟨(x, z), lt_trans h.1 h.2⟩ ≤ μ ⟨(x, y), h.1⟩ := h2 ▸ le_top
-        cases' le_iff_eq_or_lt.1 h3 with h3 h3
+        cases' le_iff_eq_or_lt.1 (h2 ▸ le_top : μ ⟨(x, z), lt_trans h.1 h.2⟩ ≤ μ ⟨(x, y), h.1⟩) with h3 h3
         · rcases μQuotient_helper r d ⟨(x,z),lt_trans h.1 h.2⟩ (h4 ▸ h'''.2) with ⟨w,⟨hw₁,_⟩⟩
           simp [μ] at h3; simp [μ] at h2
           exact False.elim (not_top_lt ((h2 ▸ h3 ▸ hw₁).symm ▸ not_top_of_Nontrivial_TotallyOrderedRealVectorSpace w))
         · refine Or.inr <| Or.inl <| ⟨h3,?_⟩
           simp [μ,μQuotient,Eq.mpr (id (congrArg (fun _a ↦ _a > 0) h4)) h'''.right,h'''.2]
-          rw [← h4]
-          exact (smul_lt_smul_iff_of_pos_left <| Right.inv_pos.mpr h').2 <| (h₁ x y z h).1 ▸ lt_add_of_pos_left (d ⟨(y, z), h.right⟩) <| h₂ x y h.1 h'''.1
+          exact h4 ▸ ((smul_lt_smul_iff_of_pos_left <| Right.inv_pos.mpr h').2 <| (h₁ x y z h).1 ▸ lt_add_of_pos_left (d ⟨(y, z), h.right⟩) <| h₂ x y h.1 h'''.1)
       · apply not_and_or.1 at h''
         apply not_and_or.1 at h'''
         simp [pos_iff_ne_zero.symm] at h'''
         have : r ⟨(y, z), h.2⟩ = 0 := by aesop
         have this' := (add_zero <| r ⟨(x, y), h.1⟩) ▸ (this ▸ (h₁ x y z h).2) ▸ h'
         have h2 : μ ⟨(y, z), h.2⟩ = ⊤ := by simp [μ, μQuotient, this]
-        have h3 : μ ⟨(x, z), lt_trans h.1 h.2⟩ ≤ μ ⟨(y, z), h.2⟩ := h2 ▸ le_top
         have h4 := (add_zero <| r ⟨(x, y), h.1⟩) ▸ this ▸ (h₁ x y z h).2
-        cases' le_iff_eq_or_lt.1 h3 with h3 h3
+        cases' le_iff_eq_or_lt.1 (h2 ▸ le_top : μ ⟨(x, z), lt_trans h.1 h.2⟩ ≤ μ ⟨(y, z), h.2⟩) with h3 h3
         · rcases μQuotient_helper r d ⟨(x,z),lt_trans h.1 h.2⟩ (h4 ▸ this') with ⟨w,hw₁,_⟩
           simp [μ] at h3; simp [μ] at h2
           exact False.elim (not_top_lt ((h2 ▸ h3 ▸ hw₁).symm ▸ not_top_of_Nontrivial_TotallyOrderedRealVectorSpace w))
         · refine Or.inl <| ⟨?_,h3⟩
           simp [μ,μQuotient,this',Eq.mpr (id (congrArg (fun _a ↦ _a > 0) h4))]
-          rw [← h4]
-          exact (smul_lt_smul_iff_of_pos_left <| Right.inv_pos.mpr h').2 <| (h₁ x y z h).1 ▸ lt_add_of_pos_right (d ⟨(x, y), h.1⟩) <| h₂ y z h.2 this
+          exact h4 ▸ ((smul_lt_smul_iff_of_pos_left <| Right.inv_pos.mpr h').2 <| (h₁ x y z h).1 ▸ lt_add_of_pos_right (d ⟨(x, y), h.1⟩) <| h₂ y z h.2 this)
 end impl
