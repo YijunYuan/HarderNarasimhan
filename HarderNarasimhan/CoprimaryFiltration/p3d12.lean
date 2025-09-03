d1d8c5bec700d556b4372dd65c83ddc6130039bc
YijunYuan <941201yuan@gmail.com>
2025-09-03 16:01:18 +0800

    新增多个引理以增强 p3d12.lean 文件的功能，并引入对支持的相关模块的引用

--- HarderNarasimhan/CoprimaryFiltration/p3d12.lean
+++ HarderNarasimhan/CoprimaryFiltration/p3d12.lean
@@ -8,6 +8,7 @@ import Mathlib.Algebra.Module.Submodule.Defs
 import Mathlib.RingTheory.Ideal.AssociatedPrime.Finiteness
 import Mathlib.Algebra.Module.LocalizedModule.Submodule
 import Mathlib.Algebra.Module.LocalizedModule.AtPrime
+import Mathlib.RingTheory.Support
 
 import HarderNarasimhan.Basic
 import HarderNarasimhan.Convexity.Results
@@ -120,18 +121,76 @@ lemma noname {R : Type} [CommRing R] [IsNoetherianRing R]
     apply S₀_order.1
     exact Set.toFinset_subset_toFinset.mpr <| assinc (lt_of_le_of_ne hu1.1.1 hu1.2) hu1.1.2
 
+lemma mem_support_of_mem_associatedPrimes {R : Type} [CommRing R]
+{M : Type} [AddCommGroup M] [Module R M] {x : Ideal R}:
+(hx : x ∈ associatedPrimes R M) →  {asIdeal := x, isPrime := hx.out.1} ∈  Module.support R M := by
+  intro hx
+  apply Module.mem_support_iff_exists_annihilator.2
+  rcases hx with ⟨p,m,hpm⟩
+  use m
+  simp only [hpm]
+  intro z hz
+  exact (Submodule.mem_annihilator_span_singleton m z).mp hz
+
+lemma TBA' {R : Type} [CommRing R]
+{M : Type} [AddCommGroup M] [Module R M]
+(N₁ N₂ N₃ : Submodule R M) (h : N₁ ≤ N₂)
+: Module.support R (N₃⧸ N₂.submoduleOf N₃) ⊆ Module.support R (N₃⧸ N₁.submoduleOf N₃) := by
+  intro p hp
+  simp [Module.mem_support_iff_exists_annihilator] at *
+  rcases hp with ⟨m,hm⟩
+  use Submodule.Quotient.mk m.out
+  intro z hz
+  have : z • (Submodule.Quotient.mk m.out : ↥N₃ ⧸ N₁.submoduleOf N₃)= 0 := by exact
+    (Submodule.mem_annihilator_span_singleton (Submodule.Quotient.mk (Quotient.out m)) z).mp hz
+  have : z ∈ (Submodule.span R {m}).annihilator := by
+    rw [Submodule.mem_annihilator_span_singleton]
+    rw [← Submodule.Quotient.mk_smul] at this
+    apply (Submodule.Quotient.mk_eq_zero _).1 at this
+    have this' : z • m = Submodule.Quotient.mk (z • m).out := by
+      unfold Submodule.Quotient.mk Quotient.mk''
+      rw [Quotient.out_eq]
+    rw [this']
+    apply (Submodule.Quotient.mk_eq_zero _).2
+    have this' : z • m.out - (z • m).out ∈ N₂.submoduleOf N₃ := by
+      apply (Submodule.Quotient.mk_eq_zero _).1
+      simp only [Submodule.Quotient.mk_sub, Submodule.Quotient.mk_smul]
+      unfold Submodule.Quotient.mk Quotient.mk''
+      rw [Quotient.out_eq, Quotient.out_eq, sub_self]
+    exact (Submodule.sub_mem_iff_right (N₂.submoduleOf N₃) (h this)).mp this'
+  exact hm this
+
+lemma min_associated_prime_iff_min_supp {R : Type} [CommRing R] [IsNoetherianRing R]
+{M : Type} [AddCommGroup M] [Module R M] [Module.Finite R M]
+{I : PrimeSpectrum R} :
+Minimal (fun J ↦ J ∈ associatedPrimes R M) I.asIdeal ↔ Minimal (fun J ↦ J ∈ Module.support R M) I := by
+  sorry
+
+lemma exists_minimal_prime_contained_supp {R : Type} [CommRing R] [IsNoetherianRing R]
+{M : Type} [AddCommGroup M] [Module R M] [Module.Finite R M] :
+∀ q : PrimeSpectrum R, q ∈ Module.support R M → ∃ p : PrimeSpectrum R, Minimal (fun J ↦ J ∈ Module.support R M) p ∧ p ≤ q := by sorry
+
 lemma prop3d12p1 {R : Type} [CommRing R] [IsNoetherianRing R]
 {M : Type} [Nontrivial M] [AddCommGroup M] [Module R M] [Module.Finite R M]
 (I : {z: (ℒ R M) × (ℒ R M) // z.1 < z.2})
-(N'' : ℒ R M) (ha1 : InIntvl I N'') (ha2 : N'' ≠ I.val.2) : ∀ q : Ideal R, (hq : q ∈ associatedPrimes R (I.val.2⧸N''.submoduleOf I.val.2)) → {asIdeal := q, isPrime := hq.out.1 } ≥ (((_μ R M) I).toFinset.min' (μ_nonempty I)) := sorry
+(N'' : ℒ R M) (ha1 : InIntvl I N'') (ha2 : N'' ≠ I.val.2) : ∀ q : Ideal R, (hq : q ∈ associatedPrimes R (I.val.2⧸N''.submoduleOf I.val.2)) → {asIdeal := q, isPrime := hq.out.1 } ≥ (((_μ R M) I).toFinset.min' (μ_nonempty I)) := by
+  intro q hq
+  have hq' := TBA' I.val.1 N'' I.val.2 (ha1.1) <| mem_support_of_mem_associatedPrimes hq
+  obtain ⟨r,hr,hr'⟩ := exists_minimal_prime_contained_supp {asIdeal := q, isPrime := hq.out.1 } hq'
+  rw [← min_associated_prime_iff_min_supp] at hr
+  have := (((_μ R M) I).toFinset.min'_le) r (by
+    simp only [Set.mem_toFinset, Set.mem_setOf_eq]
+    use r.asIdeal, hr.1
+    )
+  exact le_trans this <|  toLinearExtension.monotone' hr'
 
 noncomputable abbrev ker_of_quot_comp_localization {R : Type} [CommRing R] [IsNoetherianRing R]
 {M : Type} [Nontrivial M] [AddCommGroup M] [Module R M] [Module.Finite R M]
 (I : {z: (ℒ R M) × (ℒ R M) // z.1 < z.2})
 : ℒ R M := by
-  let f1 := Submodule.toLocalized (((_μ R M) I).toFinset.min' (μ_nonempty I)).asIdeal.primeCompl  (⊤ : Submodule R (I.val.2⧸I.val.1.submoduleOf I.val.2))
-  let f2 : I.val.2 →ₗ[R] (⊤ : Submodule R (I.val.2⧸I.val.1.submoduleOf I.val.2)) :=
-    { toFun := fun x : I.val.2 ↦ ⟨(I.val.1.submoduleOf I.val.2).mkQ x, trivial⟩,
+  let f1 := LocalizedModule.mkLinearMap (((_μ R M) I).toFinset.min' (μ_nonempty I)).asIdeal.primeCompl (I.val.2⧸I.val.1.submoduleOf I.val.2)
+  let f2 : I.val.2 →ₗ[R] (I.val.2⧸I.val.1.submoduleOf I.val.2) :=
+    { toFun := fun x : I.val.2 ↦ (I.val.1.submoduleOf I.val.2).mkQ x,
       map_add' := fun x y => rfl,
       map_smul' := fun r x => rfl }
   exact Submodule.map I.val.2.subtype (LinearMap.ker (f1 ∘ₗ f2))
@@ -181,7 +240,37 @@ lemma prop3d12 {R : Type} [CommRing R] [IsNoetherianRing R]
       simp only [(TBA I).2, Set.mem_singleton_iff, exists_prop_eq, Set.setOf_eq_eq_singleton',
         Set.toFinset_singleton, Finset.singleton_inj]
       rfl
-    · exact (TBA I).1
+    · constructor
+      · constructor
+        · unfold ker_of_quot_comp_localization
+          simp only [Submodule.mkQ_apply]
+          intro z hz
+          simp only [Submodule.mem_map, LinearMap.mem_ker, LinearMap.coe_comp, LinearMap.coe_mk,
+            AddHom.coe_mk, Function.comp_apply, Submodule.subtype_apply, Subtype.exists,
+            exists_and_right, exists_eq_right]
+          use (le_of_lt I.prop) hz
+          have : Submodule.Quotient.mk ⟨z, (Iff.of_eq (Eq.refl (z ∈ I.val.2))).mpr (le_of_lt (Subtype.prop I) hz) ⟩ = (0 : ↥I.val.2 ⧸ Submodule.submoduleOf I.val.1 I.val.2) := by
+            simp only [Submodule.Quotient.mk_eq_zero]
+            exact hz
+          simp only [this]
+          exact rfl
+        · unfold ker_of_quot_comp_localization
+          simp only [Submodule.map_subtype_le]
+      · by_contra hc
+        have := (((_μ R M) I).toFinset.min'_mem (μ_nonempty I))
+        simp only [Set.mem_toFinset, Set.mem_setOf_eq] at this
+        rcases this with ⟨p,⟨hp1,hp2⟩⟩
+        apply mem_support_of_mem_associatedPrimes at hp1
+        have hp1 := hp1.out
+        have : LinearMap.ker (LocalizedModule.mkLinearMap (((_μ R M) I).toFinset.min' (μ_nonempty I)).asIdeal.primeCompl (I.val.2⧸I.val.1.submoduleOf I.val.2)) ≠ ⊤ := by
+          by_contra hc
+          apply LocalizedModule.subsingleton_iff_ker_eq_top.2 at hc
+          rw [hp2] at hp1
+          exact false_of_nontrivial_of_subsingleton (LocalizedModule ((_μ R M I).toFinset.min' (μ_nonempty I)).asIdeal.primeCompl (↥I.val.2 ⧸ Submodule.submoduleOf I.val.1 I.val.2))
+
+
+
+        sorry
   apply IsLeast.csInf_eq
   refine ⟨res1,?_⟩
   apply mem_lowerBounds.2
