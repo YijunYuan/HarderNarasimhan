c281ec897ef24ff57257c3caebd7cf621576ee38
Yijun Yuan <941201yuan@gmail.com>
2025-09-20 19:11:47 +0800

    refactor: Update length_eq_of_JordanHolderFiltration theorem to use eq_of_le_of_ge

--- HarderNarasimhan/JordanHolderFiltration/Results.lean
+++ HarderNarasimhan/JordanHolderFiltration/Results.lean
@@ -82,7 +82,7 @@ theorem length_eq_of_JordanHolderFiltration {ℒ : Type*} [Nontrivial ℒ] [Latt
 ------------
 ∀ JH1 JH2 : JordanHolderFiltration μ, Nat.find JH1.fin_len = Nat.find JH2.fin_len
 ------------
-:= fun JH1 JH2 ↦ eq_of_le_of_le (impl.induction_on_length_of_JordanHolderFiltration (Nat.find JH2.fin_len) ℒ _ _ _ inferInstance inferInstance _ _ μ inferInstance inferInstance inferInstance inferInstance inferInstance ⟨JH2,rfl.le⟩ JH1) <| impl.induction_on_length_of_JordanHolderFiltration (Nat.find JH1.fin_len) ℒ _ _ _ inferInstance inferInstance _ _ _ inferInstance inferInstance inferInstance inferInstance inferInstance ⟨JH1,rfl.le⟩ JH2
+:= fun JH1 JH2 ↦ eq_of_le_of_ge (impl.induction_on_length_of_JordanHolderFiltration (Nat.find JH2.fin_len) ℒ _ _ _ inferInstance inferInstance _ _ μ inferInstance inferInstance inferInstance inferInstance inferInstance ⟨JH2,rfl.le⟩ JH1) <| impl.induction_on_length_of_JordanHolderFiltration (Nat.find JH1.fin_len) ℒ _ _ _ inferInstance inferInstance _ _ _ inferInstance inferInstance inferInstance inferInstance inferInstance ⟨JH1,rfl.le⟩ JH2
 
 
 end HarderNarasimhan
